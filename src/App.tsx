f09b7ff75f17088426bb19b804ea3f5cabcef165
mounika gonti <hmounika1997@gmail.com>
2024-05-21 20:39:38 +0530

    error modified

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
-import  { FC } from 'react';
-import { Provider } from 'react-redux';
-import { store } from './Redux/store';
-import ThemeWrapper from './Config/ThemeWrapper';
+import {FC, Suspense} from 'react'
+import {Provider} from 'react-redux'
+import {store} from './Redux/store'
+import ThemeWrapper from './Config/ThemeWrapper'
+import {FiLoader} from 'react-icons/fi'
 
 const App: FC = () => {
   return (
-    <Provider store={store}>
-      <ThemeWrapper />
-    </Provider>
-  );
-};
+    <Suspense fallback={<FiLoader />}>
+      <Provider store={store}>
+        <ThemeWrapper />
+      </Provider>
+    </Suspense>
+  )
+}
 
-export default App;
+export default App
