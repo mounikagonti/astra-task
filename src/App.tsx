218e49bc3173712c122267f0976327917ad7c51d
mounika gonti <hmounika1997@gmail.com>
2024-05-22 06:54:14 +0530

    file changed

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,13 @@
-import {FC, Suspense} from 'react'
+import {FC} from 'react'
 import {Provider} from 'react-redux'
 import {store} from './Redux/store'
 import ThemeWrapper from './Config/ThemeWrapper'
-import {FiLoader} from 'react-icons/fi'
 
 const App: FC = () => {
   return (
-    <Suspense fallback={<FiLoader />}>
-      <Provider store={store}>
-        <ThemeWrapper />
-      </Provider>
-    </Suspense>
+    <Provider store={store}>
+      <ThemeWrapper />
+    </Provider>
   )
 }
 
